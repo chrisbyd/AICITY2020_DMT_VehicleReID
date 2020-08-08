c077cfe306ec26449e9ff8df9dc03b1f39806cb7
Shawn Zhang <zhangsheng1730@hotmail.com>
2020-08-08 23:41:09 +0800

    add load model

--- model/make_model.py
+++ model/make_model.py
@@ -5,6 +5,19 @@ from loss.arcface import ArcFace
 from .backbones.resnet_ibn_a import resnet50_ibn_a,resnet101_ibn_a
 from .backbones.se_resnet_ibn_a import se_resnet101_ibn_a
 import torch.nn.functional as F
+from torch.hub import load_state_dict_from_url
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+    'resnext50_32x4d': 'https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth',
+    'resnext101_32x8d': 'https://download.pytorch.org/models/resnext101_32x8d-8ba56ff5.pth',
+    'wide_resnet50_2': 'https://download.pytorch.org/models/wide_resnet50_2-95faca4d.pth',
+    'wide_resnet101_2': 'https://download.pytorch.org/models/wide_resnet101_2-32ee1156.pth',
+}
 
 def weights_init_kaiming(m):
     classname = m.__class__.__name__
@@ -63,7 +76,12 @@ class Backbone(nn.Module):
             print('unsupported backbone! but got {}'.format(model_name))
 
         if pretrain_choice == 'imagenet':
-            self.base.load_param(model_path)
+            try:
+                self.base.load_param(model_path)
+            except:
+                state_dict = load_state_dict_from_url(model_urls[model_name], model_dir=model_path)
+                self.base.load_param(model_path)
+
             print('Loading pretrained ImageNet model......from {}'.format(model_path))
 
         self.gap = nn.AdaptiveAvgPool2d(1)
